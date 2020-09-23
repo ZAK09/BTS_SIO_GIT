4c624ad3593e6dd1795459f343663c57bc3439b4
Zakaria Mejdoub <mejdoub-zakaria@gmx.fr>
2020-09-23 10:50:08 +0200

    plusoumoins

---
+++ Main.java
@@ -0,0 +1,29 @@
+package com.test;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int n = (int)(Math.random() * 101);
+        System.out.println(n);
+        int nbplayer;
+
+        do {
+            Scanner myObj = new Scanner(System.in);
+            System.out.println("Entrez un nombre: ");
+            nbplayer = myObj.nextInt();
+
+            if (nbplayer < n) {
+                System.out.println("C'est plus!");
+            }
+            else if (nbplayer > n) {
+                System.out.println("C'est moins!");
+
+            }
+        }
+        while (nbplayer != n);
+
+        System.out.println("Bravo! Vous avez trouvé le nombre aléatoire!");
+    }
+}
